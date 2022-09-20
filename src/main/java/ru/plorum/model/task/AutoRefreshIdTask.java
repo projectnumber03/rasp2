967dba8cfa3ad5f98b0851b0f6c32c8b602948f1
sdv <abarakus@bk.ru>
2022-09-20 23:26:18 +0300

    removed "refreshing id's" log message

--- src/main/java/ru/plorum/model/task/AutoRefreshIdTask.java
+++ src/main/java/ru/plorum/model/task/AutoRefreshIdTask.java
@@ -1,6 +1,5 @@
 package ru.plorum.model.task;
 
-import org.apache.logging.log4j.LogManager;
 import ru.plorum.model.Device;
 import ru.plorum.service.PropertiesService;
 
@@ -17,7 +16,6 @@ public class AutoRefreshIdTask implements Task {
     @Override
     public Runnable getBody() {
         return () -> {
-            LogManager.getLogger(this.getClass()).info("refreshing id's");
             getDevices().forEach(Device::refreshId);
             PropertiesService.INSTANCE.updateDevicesId(getDevices().stream().map(Device::getId).map(UUID::toString).collect(Collectors.toList()));
         };
